8bcad33b9c89fe54c5b07593151964c5da2e9222
Vian Tibay <202310591@fit.edu.ph>
2025-07-09 05:56:31 +0800

    feat(fFile structure):Add initial implementation of Inventory System with HTML structure, CSS styling, and JavaScript functionality

---
+++ Index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" />
+    <link rel="stylesheet" href="./Assets/css/style.css">
+    <title>Inventory System</title>
+</head>
+<body>
+    <div class ="header">
+    <h1>Welcome to the Inventory System</h1>
+    <p>This is a simple inventory management system.</p>
+    </div>
+    <ul>
+        <li>
+            <a href="add_item.php">Add Item</a></li>
+        <li>
+            <a href="view_items.php">View Items</a></li>
+        <li>
+            <a href="update_item.php">Update Item</a></li>
+        <li>
+            <a href="delete_item.php">Delete Item</a></li>
+    </ul> 
+
+</body>
+</html>
